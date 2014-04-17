622a1d1241c11431bb5e19473760d4f56e328f5a
Babak Toghiani-Rizi <babaktr@Babaks-MacBook-Pro.local>
2014-04-17 16:31:37 +0200

    fixat i request-delen

---
+++ app/projekt/projekt/CustomCell.m
@@ -0,0 +1,96 @@
+//
+//  CustomCell.m
+//  projekt
+//
+//  Created by Babak Toghiani-Rizi on 17/04/14.
+//  Copyright (c) 2014 OSM-projekt. All rights reserved.
+//
+
+#import "CustomCell.h"
+
+
+@implementation CustomCell
+
+@synthesize primaryLabel, primaryLabelTwo, distanceLabel, myImageView, mySecondImageView;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+
+- (id)initWithFrame:(CGRect)frame reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithFrame:frame reuseIdentifier:reuseIdentifier]) {
+        // Initialization code
+        primaryLabel = [[UILabel alloc]init];
+        primaryLabel.textAlignment = UITextAlignmentLeft;
+        primaryLabel.font = [UIFont boldSystemFontOfSize:18];
+        primaryLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        
+        primaryLabelTwo = [[UILabel alloc]init];
+        primaryLabelTwo.textAlignment = UITextAlignmentLeft;
+        primaryLabelTwo.font = [UIFont systemFontOfSize:12];
+        primaryLabelTwo.lineBreakMode = NSLineBreakByWordWrapping;
+        
+        distanceLabel = [[UILabel alloc]init];
+        distanceLabel.textAlignment = UITextAlignmentLeft;
+        distanceLabel.font = [UIFont boldSystemFontOfSize:20];
+        distanceLabel.lineBreakMode = NSLineBreakByWordWrapping;
+
+        
+        myImageView = [[UIImageView alloc]init];
+    
+        mySecondImageView = [[UIImageView alloc]init];
+        
+        [self.contentView addSubview:primaryLabel];
+        [self.contentView addSubview:primaryLabelTwo];
+        [self.contentView addSubview:distanceLabel];
+        [self.contentView addSubview:myImageView];
+        [self.contentView addSubview:mySecondImageView];
+        
+        
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    CGRect contentRect = self.contentView.bounds;
+    CGFloat boundsX = contentRect.origin.x;
+    CGRect frame;
+    frame= CGRectMake(boundsX+260 ,35, 34, 34);
+    myImageView.frame = frame;
+    
+    frame= CGRectMake(boundsX+5 ,2, 50, 50);
+    mySecondImageView.frame = frame;
+    
+    frame= CGRectMake(boundsX+15 ,10, 250, 20);
+    primaryLabel.frame = frame;
+    
+    frame= CGRectMake(boundsX+160-35 ,30, 70, 25);
+    primaryLabelTwo.frame = frame;
+    
+    frame= CGRectMake(boundsX+160-35 ,50, 70, 25);
+    distanceLabel.frame = frame;
+
+    
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50;
+}
+
+@end
