17b563502625bb13b7e2bab0e32e961620f220f3
Max Reeves <Max.Reeves.1563@student.uu.se>
2014-04-22 15:12:00 +0200

    Listener spawns new listener on connection and accepts multiple messages

--- server/app/core/listener.erl
+++ server/app/core/listener.erl
@@ -25,16 +25,20 @@ connect(initialized, ListenSocket) ->
     case gen_tcp:accept(ListenSocket) of
 	{ok, AcceptSocket} ->
 	    io:format("Connected~n"),
+	    listener_sup:start_listener(),
 	    {next_state, login, AcceptSocket};
 	{error, Reason} ->
 	    io:format("Terminating~n"),
+	    listener_sup:start_listener(),
 	    {stop, Reason, ListenSocket} 
     end.
 
 handle_info({tcp, _Socket, Packet}, login, AcceptSocket) ->
+    inet:setopts(AcceptSocket, [{active, once}]),
     io:format("Received message: ~s~n", [binary_to_list(Packet)]),
-    gen_tcp:send(AcceptSocket, Packet),    
+    gen_tcp:send(AcceptSocket, Packet),
     {next_state, login, AcceptSocket}.
 
-terminate(_Reason, _Socket) ->
+terminate(_Reason, Socket) ->
+    gen_tcp:close(Socket),
     ok.
