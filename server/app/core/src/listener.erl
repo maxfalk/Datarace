56923dc7b57ef77561b7bc2c45b76ba6cb5e9483
Max Reeves <Max.Reeves.1563@student.uu.se>
2014-04-23 14:10:04 +0200

    Cleaned up the logic for listener a bit

--- server/app/core/src/listener.erl
+++ server/app/core/src/listener.erl
@@ -5,7 +5,11 @@
 -behaviour(gen_fsm).
 
 -export([start_link/1]).
--export([init/1, connect/2, terminate/3, handle_info/3]).
+-export([init/1, connect/2, login/2, terminate/3, handle_info/3]).
+
+-define(LOGIN_INFO, <<0>>).
+-define(LOGIN_REPLY, <<1>>).
+-define(REGISTER_INFO, <<2>>).
 
 
 %% Server API
@@ -32,22 +36,31 @@ connect(initialized, ListenSocket) ->
 	    {stop, Reason, ListenSocket} 
     end.
 
-handle_info({tcp, _Socket, Packet}, login, AcceptSocket) ->
+login({?LOGIN_INFO, Packet}, AcceptSocket) ->
     inet:setopts(AcceptSocket, [{active, once}]),
-    io:format("Received message: ~s~n", [binary_to_list(Packet)]),
-    gen_tcp:send(AcceptSocket, Packet),
-    {next_state, login, AcceptSocket};
-handle_info({tcp_closed, _Socket}, _State, AcceptSocket) ->
-    gen_tcp:close(AcceptSocket),
+    case Packet of
+	<<"login">> -> 
+	    gen_tcp:send(AcceptSocket, <<"You are logged in">>),
+	    io:format("Logged in~n"),
+	    {next_state, login, AcceptSocket};
+	_Packet -> 
+	    gen_tcp:send(AcceptSocket, <<"Login failed">>),
+	    io:format("Login in failed~n"),
+	    {next_state, login, AcceptSocket}
+    end;
+login(Packet, AcceptSocket) ->
+    inet:setopts(AcceptSocket, [{active, once}]),
+    io:format("Received weird message: ~w~n", [Packet]),
     {stop, normal, AcceptSocket}.
 
-terminate(_Reason, init, _ListenSocket) ->
-    io:format("Terminating~n"),
-    ok;
-terminate(_Reason, connect, _ListenSocket) ->
-    io:format("Terminating~n"),
-    ok;
-terminate(_Reason, _State, AcceptSocket) ->
-    io:format("Terminating~n"),
-    gen_tcp:close(AcceptSocket),
-    ok.
+handle_info({tcp, _, <<Type:1/binary, Packet/binary>>}, State, Socket) ->
+    ?MODULE:State({Type, Packet}, Socket);
+handle_info({tcp, _t, <<Type:1/binary>>}, State, Socket) ->
+    ?MODULE:State(Type, Socket);
+handle_info({tcp_closed, _}, _State, Socket) ->
+    {stop, normal, Socket};
+handle_info({tcp_error, _, _Reason}, _State, Socket) ->
+    {stop, normal, Socket}.
+
+terminate(_Reason, _State, _Socket) ->
+    io:format("Terminating~n").
