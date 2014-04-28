1db9234cd69267e4ca0e8e808b80c237403d2d18
Max Reeves <Max.Reeves.1563@student.uu.se>
2014-04-28 13:30:11 +0200

    Added documentation

--- server/app/datarace/src/master_sup.erl
+++ server/app/datarace/src/master_sup.erl
@@ -1,5 +1,7 @@
 
+%%====================================================================
 %% master_sup
+%%====================================================================
 
 -module(master_sup).
 -behaviour(supervisor).
@@ -8,13 +10,30 @@
 -export([init/1]).
 
 
+%%====================================================================
 %% Supervisor API
+%%====================================================================
+
+%% @doc Start a new Master supervisor. 
+
+-spec start_link() -> Result when
+      Result :: {ok, pid()} | 
+		ignore | 
+		{error, Error},
+      Error :: {already_started, pid()} | 
+	       {shutdown, term()} | 
+	       term().
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 
+%%====================================================================
 %% Callback functions
+%%====================================================================
+
+%% @doc Initializes the Master supervisor by first starting a 
+%% Client_serv supervisor and the a Listener supervisor.
 
 init(_Args) ->
     Port = 8888,
