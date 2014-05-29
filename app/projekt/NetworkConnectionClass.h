bb633076514d0c5d277a4c25756ae478fb8bdc0d
Joel Sandberg <j.sandberg@hotmail.com>
2014-05-29 14:55:36 +0200

    Dokumenterat upp den kod som inte vart kommenterats

--- app/projekt/NetworkConnectionClass.h
+++ app/projekt/NetworkConnectionClass.h
@@ -107,23 +107,82 @@ typedef struct __attribute__ ((packed)) {
  */
 +(void)signOut;
 
+/**
+ This method gets stats from the past races
+ @method getHomeStats
+ 
+ */
 +(void *)getHomeStats;
-
+/**
+ This method sends race requests to opponents
+ @method sendRequest
+ 
+ 
+ */
 +(void)sendRequest;
+/**
+ This method gets the requests from the server
+ @method getRequest
+ 
+ */
 +(void *)getRequests:(int)type1 type2:(int)type2;
-
+/**
+ This method sends an accept to the server 
+ @method acceptRequest
+ @param requestId The ID of the accepted request
+ */
 +(int)acceptRequest:(uint32_t) requestId;
+/**
+ This method send a package telling the server to cancel the request
+ @method cancelMethod
+ @param requestId The ID of the declined request
+ */
 +(int)cancelRequest:(uint32_t) requestId;
 
+/**
+ This method is used when a user wants to send a race request
+ @method makeRequest
+ @param userId the Id of the user you want to challenge
+ @param distance The distance you want to challenge the opponent with
+ 
+ */
 +(int)makeRequest:(uint32_t) userId distance:(uint32_t)distance;
+/**
+ This method lets the user to searh for other users
+ @method searhForUsers
+ @param username The name of the opponent the user wants to find
+ */
+
 
 +(void *)searchForUsers:(NSString *)username;
+/**
+ This method starts to send the data to the server when a race starts
+ @method startRace 
+ @param reqID the ID of the user thats starts to run
+ 
+ */
 
 +(int)startRace:(int)reqID;
+/**
+ This sends the coordinates to the server
+ @method
+ @param latitude 
+ @param longitude
+ 
+ */
 
 +(void)sendUpdatedCoordinates:(double)latitude longitude:(double)longitude;
 
+/**
+ This method is called when a user wants to quit the race
+@method quitRace
+ */
+
 +(void)quitRace;
+/**
+ This method gets the distance that the competitors has gone from the server
+ @method requestCompetitorDistance
+ */
 
 +(void *)requestCompetitorsDistance;
 
